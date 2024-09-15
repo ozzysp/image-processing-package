223503d186b0855885faf4be6d11de696dae8eb8
Ozzy <ozzy@ozzy.local>
2024-09-15 12:32:48 -0300

    Modifies setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     page_description = f.read()
 
 with open("requirements.txt") as f:
@@ -8,14 +8,21 @@ with open("requirements.txt") as f:
 
 setup(
     name="ozzy_image_processing",
-    version="0.0.1",
-    author="ozzy_azevedo",
-    author_email="",
-    description="Package to processing images",
+    version="0.0.2",
+    author="Ozzy Azevedo",
+    author_email="ozzysp@icloud.com",
+    description="Package to process images using Python",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ozzysp/image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+    ],
 )

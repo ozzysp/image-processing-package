abbc14fffd51330a37a07411412732ce3bfacdb5
Ozzy <ozzy@ozzy.local>
2024-09-15 05:03:27 -0300

    Modifies setup.py file

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="ozzy_azevedo",
+    author_email="",
+    description="Package to processing images",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ozzysp/image-processing-package"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',

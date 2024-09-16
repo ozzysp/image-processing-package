c297d12ee072a3d4d1469ed717dae2beb0963429
Ozzy <ozzy@ozzy.local>
2024-09-16 08:10:14 -0300

    Fix minor errors

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    page_description = f.read()
+# Leitura do README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+# Leitura do requirements.txt
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
 
 setup(
-    name="ozzy_image_processing",
-    version="0.0.2",
+    name="ozzy_image_processing",  # nome do pacote
+    version="0.0.3",  # versão do pacote
     author="Ozzy Azevedo",
     author_email="ozzysp@icloud.com",
     description="Package to process images using Python",
-    long_description=page_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ozzysp/image-processing-package",
-    packages=find_packages(),
-    install_requires=requirements,
-    python_requires='>=3.8',
+    long_description=long_description,  # conteúdo do README
+    long_description_content_type="text/markdown",  # formato do README
+    url="https://github.com/ozzysp/image-processing-package",  # link do projeto
+    packages=find_packages(),  # localizar pacotes no projeto
+    install_requires=requirements,  # instalar as dependências
+    python_requires='>=3.8',  # versão mínima do Python
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
